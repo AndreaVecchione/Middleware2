4c24f4611f38a082c022187f9263427e77e591b5
Andrea Vecchione <andrea.vecchione02@gmail.com>
2024-10-09 15:53:58 +0200

    Middleware2

---
+++ src/main/java/com/example/Middleware2/configurations/SpringMvcConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.Middleware2.configurations;
+
+import com.example.Middleware2.controllers.MonthController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class SpringMvcConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private HandlerInterceptor handlerInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(handlerInterceptor);
+    }
+}
